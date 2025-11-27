f3b8a3aeb1e7ed58032d6c55edddaddbe5f2a54b
Username-Red <ashkestrel@gmail.com>
2025-11-27 18:37:53 +0200

    fixed hydration bug in header, removed firebase deployment files

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,64 @@
 "use client";
 import Link from 'next/link';
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import SignedIn from './components/Signed-in';
 import SignedOut from './components/Signed-out';
 import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
 import { auth, database } from './firebase/config';
-
+ 
 // Firestore "tutorial" code to get document
-
+ 
 // Import Firestore functions
-import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
-
+import { getFirestore, doc, setDoc, getDoc, DocumentReference } from "firebase/firestore";
+import SignIn from './auth/login/page';
+ 
 // use doc to create a reference to the document, eg: doc(datbase, collection, documentID)
 const docRef = doc(database, "students", "3D9FVGr0i4YUxQ7SOfsH"); // collection "students", doc "3D9FVGr0i4YUxQ7SOfsH", should fetch "Marcus"
 // use getDoc to fetch the document snapshot
 const docSnap = await getDoc(docRef);
-
+ 
 const Home = () => {
   const [user, loading, error] = useAuthState(auth);
   const [signOut, signingOut, signOutError] = useSignOut(auth);
-
-  // Check if document exists
-  if (docSnap.exists()) {
-    // Document data found, you can use it
-    console.log(docSnap.data());
-  } 
-  // Document does not exist
-  else {
-    // doc.data() will be undefined in this case, so log no document
-    console.log("No such document!");
+ 
+  // When login is being checked
+  if (loading) {
+    return <p >Checking Login...</p>;
   }
-
-  // tutorial complete
-
-
+ 
+  // If user is not logged in, show the login form
+  if (!user) {
   return (
     <>
     <div>
       <h1>Welcome to Student Data Management System</h1>
-      <p>Manage student records table coming soon</p>
+      <p>Please Login to Manage Student Records</p>
       <p>{}</p>
+      </div>
+     
+      <div className='mt-4'>
+        <SignIn />
+      </div>
+     
+    <p className="mt-4">
+          Don't have an account?  
+          <Link href="/signup" className="underline text-blue-600">Sign up</Link>
+        </p>
+      </>
+    );
+  }
+ 
+  // Loggin and have access to student data
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Welcome, {user.email}</h1>
+      <p className="mt-2">You are logged in.</p>
+ 
+      <Link href="/students" className="btn btn-primary mt-4">
+        Go to Student Records
+      </Link>
     </div>
-    </>
-  )
+  );
 }
-
+ 
 export default Home
