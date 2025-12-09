7ce2b3a4eddf7aade34fdaf02e6d1b9b85678a81
Username-Red <ashkestrel@gmail.com>
2025-12-09 17:56:56 +0200

    Updated dakr/light theme, fixed hydration error

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/config";
@@ -7,31 +8,49 @@ import { signOut } from "firebase/auth";
 import ThemeBtn from "./ThemeBtn";
 
 const Header = () => {
+  const [mounted, setMounted] = useState(false);
   const [user] = useAuthState(auth);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <div className="navbar bg-base-100 shadow-sm flex justify-between">
+    <div className="navbar bg-base-100 text-base-content shadow-md px-4">
       {/* Left Side */}
       <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">Student Data Manager</a>
+        <span className="btn btn-ghost text-xl">
+          Student Data Manager
+        </span>
       </div>
 
       {/* Right Side */}
       <div className="navbar-end flex items-center gap-4">
-
         {user ? (
           <>
-            <span className="font-semibold">Welcome, {user.email}</span>
-            <button className="btn btn-neutral" onClick={() => signOut(auth)}>
+            <span className="font-semibold">
+              Welcome, {user.email}
+            </span>
+            <button
+              className="btn btn-neutral"
+              onClick={() => signOut(auth)}
+            >
               Sign Out
             </button>
           </>
         ) : (
           <>
-            <Link href="/auth/signup" className="btn mr-2">Sign Up</Link>
-            <Link href="/auth/login" className="btn">Login</Link>
+            <Link href="/auth/signup" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link href="/auth/login" className="btn btn-secondary">
+              Login
+            </Link>
           </>
         )}
+
         <ThemeBtn />
       </div>
     </div>
